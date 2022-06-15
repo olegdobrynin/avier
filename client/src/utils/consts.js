eeb7df3fa8175d320e97394829e9996b56774166
olegdobrynin <dobryi95@gmail.com>
2022-06-16 02:50:21 +0300

    Moving folder to other repo

--- client/src/utils/consts.js
+++
@@ -1,7 +0,0 @@
-export const ADMIN_ROUTE = '/admin'
-export const LOGIN_ROUTE = '/login'
-export const REGISTRATION_ROUTE = '/registration'
-export const MAIN_ROUTE = '/'
-export const ART_ROUTE = '/art'
-export const MARK_ROUTE = '/mark'
-export const ARTIST_ROUTE = '/artist'
